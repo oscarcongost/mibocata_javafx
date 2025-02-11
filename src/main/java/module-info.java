c0f546a3a247a152302f4f94642a3b14f984f58b
Oscar Congost <oscarsanful17@gmail.com>
2025-02-11 18:57:57 +0100

    añade modelos y daos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.example.mibocatajavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
 
 
     opens org.example.mibocatajavafx to javafx.fxml;
--- src/main/java/org/example/mibocatajavafx/models/Alumnos.java
+++ src/main/java/org/example/mibocatajavafx/models/Alumnos.java
@@ -1,4 +1,107 @@
 package org.example.mibocatajavafx.models;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Alumnos")
 public class Alumnos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "mac", nullable = false)
+    private char mac;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "motivo_baja")
+    private String motivo_baja;
+    @Column(name = "fecha_baja")
+    private Date fecha_baja;
+    @Column(name = "abonado", nullable = false)
+    private int abonado;
+    @Column(name = "pass", nullable = false)
+    private String pass;
+    @Column(name = "correo")
+    private String correo;
+
+    public Alumnos() {
+    }
+
+    public Alumnos(Long id, char mac, String nombre, String motivo_baja, Date fecha_baja, int abonado, String pass, String correo) {
+        this.id = id;
+        this.mac = mac;
+        this.nombre = nombre;
+        this.motivo_baja = motivo_baja;
+        this.fecha_baja = fecha_baja;
+        this.abonado = abonado;
+        this.pass = pass;
+        this.correo = correo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public char getMac() {
+        return mac;
+    }
+
+    public void setMac(char mac) {
+        this.mac = mac;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMotivo_baja() {
+        return motivo_baja;
+    }
+
+    public void setMotivo_baja(String motivo_baja) {
+        this.motivo_baja = motivo_baja;
+    }
+
+    public Date getFecha_baja() {
+        return fecha_baja;
+    }
+
+    public void setFecha_baja(Date fecha_baja) {
+        this.fecha_baja = fecha_baja;
+    }
+
+    public int getAbonado() {
+        return abonado;
+    }
+
+    public void setAbonado(int abonado) {
+        this.abonado = abonado;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
